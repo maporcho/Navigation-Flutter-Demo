32e1722002bbe7c362cd74ab89aa398b9c8be99a
Marco Porcho <maporcho@gmail.com>
2019-07-22 19:47:00 -0300

    routes and navigation

--- lib/screen0.dart
+++ lib/screen0.dart
@@ -15,6 +15,7 @@ class Screen0 extends StatelessWidget {
               color: Colors.red,
               child: Text('Go To Screen 1'),
               onPressed: () {
+                Navigator.pushNamed(context, '/first');
                 //Navigate to Screen 1
               },
             ),
@@ -22,7 +23,7 @@ class Screen0 extends StatelessWidget {
               color: Colors.blue,
               child: Text('Go To Screen 2'),
               onPressed: () {
-                //Navigate to Screen 2
+                Navigator.pushNamed(context, '/second');
               },
             ),
           ],
